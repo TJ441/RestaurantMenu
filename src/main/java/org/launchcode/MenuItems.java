35379d5cee580fb362f7c95799360da474f247c6
Tim Johnson <timothy.johnson@zohomail.com>
2022-12-25 06:16:56 -0600

    MenuItems filled out

--- src/main/java/org/launchcode/MenuItems.java
+++ src/main/java/org/launchcode/MenuItems.java
@@ -1,4 +1,25 @@
 package org.launchcode;
 
 public class MenuItems {
+  private String itemName;
+  private double price;
+  private String category;
+  private int daysOnMenu;
+
+  public MenuItems(String itemName, double price, String category, int daysOnMenu) {
+    this.itemName = itemName;
+    this.price = price;
+    this.category = category;
+    this.daysOnMenu = daysOnMenu;
+  }
+
+  public void setItemName(String aItemName) { itemName = aItemName; }
+  public void setPrice(double aPrice) { price = aPrice; }
+  public void setCategory(String aCategory) { category = aCategory; }
+  public void setDaysOnMenu(int aDaysOnMenu) { daysOnMenu = aDaysOnMenu; }
+
+  public String getItemName() { return itemName; }
+  public double getPrice() { return price; }
+  public String getCategory() { return category; }
+  public int getDaysOnMenu() { return daysOnMenu; }
 }
